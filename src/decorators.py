fee310f3313cf14b1a4fc4d4ce311bc6543aba49
rauf <raufasadov23@gmail.com>
2024-07-16 14:27:11 +0400

    Initial configuration

---
+++ src/decorators.py
@@ -0,0 +1,27 @@
+from functools import wraps
+from flask import request, jsonify
+from flask_login import current_user
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if current_user.is_authenticated:
+            return f(*args, **kwargs)
+        return jsonify({'message': 'Login required'}), 401
+    return decorated_function
+
+def logout_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not current_user.is_authenticated:
+            return f(*args, **kwargs)
+        return jsonify({'message': 'Logout required'}), 401
+    return decorated_function
+
+def admin_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if current_user.is_authenticated and current_user.is_admin:
+            return f(*args, **kwargs)
+        return jsonify({'message': 'Admin required'}), 401
+    return decorated_function
